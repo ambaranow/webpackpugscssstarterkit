fa75d817cbc218c4d296dab62696683386cecf91
ambaranow <a.baranov@notamedia.ru>
2018-02-01 23:03:15 +0300

    add meta tags

--- build/dev-server.js
+++ build/dev-server.js
@@ -1,4 +1,5 @@
 require('./check-versions')()
+var chalk = require('chalk')
 
 var config = require('../config')
 if (!process.env.NODE_ENV) {
@@ -72,7 +73,7 @@ var readyPromise = new Promise(resolve => {
 
 console.log('> Starting dev server...')
 devMiddleware.waitUntilValid(() => {
-  console.log('> Listening at ' + uri + '\n')
+  console.log(chalk.blue('> Listening at ' + uri + '\n'))
   // when env is testing, don't need open it
   if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
     opn(uri)
